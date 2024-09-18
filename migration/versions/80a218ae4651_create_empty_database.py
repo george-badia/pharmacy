7d369358f0af7bb0ca2966ec562b862743e6ff52
jojo <georgebadia2019@gmail.com>
2024-09-18 15:37:40 +0300

    creates tables

--- migration/versions/80a218ae4651_create_empty_database.py
+++ migration/versions/80a218ae4651_create_empty_database.py
@@ -1,7 +1,7 @@
 """Create empty database
 
 Revision ID: 80a218ae4651
-Revises: b82cfff2b6a5
+Revises: 
 Create Date: 2024-09-18 14:08:36.699627
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '80a218ae4651'
-down_revision: Union[str, None] = 'b82cfff2b6a5'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
